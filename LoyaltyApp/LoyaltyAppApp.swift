6a3a9995111c330114a2ba4a654d67d8ee7082d7
gtuazon18 <tuazon548@gmail.com>
2026-03-23 21:50:49 +0800

    Initial Commit

---
+++ LoyaltyApp/LoyaltyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoyaltyAppApp.swift
+//  LoyaltyApp
+//
+//  Created by Gilbert Tuazon on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct LoyaltyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
